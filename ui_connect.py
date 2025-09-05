ac0090ae17e79ea346044459a725e30b347ea694
kumonikumon <yoshiyoshiyoshikumo@gmail.com>
2025-09-05 14:03:58 +0900

    geminiのデータをスプシに上げるように

--- ui_connect.py
+++ ui_connect.py
@@ -7,6 +7,7 @@ from google import genai
 from google.genai import types
 from io import StringIO
 from time import sleep
+import numpy as np
 
 #スプレッドシートに入力する情報
 SPREADSHEET_ID = "1mRJj0IyX9EZ8NvV75-BAC_imRyRkF1k7skJ5sRBRz7M"
@@ -34,8 +35,7 @@ def gemini(prompt, sys):
     return response.text
 
 def ui_clear_and_go(ph, page):
-    with st.spinner("読み込み中…"):
-        sleep(0.5)
+    sleep(1)
     ph.empty()
     st.session_state.page = page
     
@@ -67,16 +67,14 @@ if "page" not in st.session_state:
 
 if st.session_state.page == "home":
     with (ph := st.empty()).container():
-        st.title("シフト管理アプリ")
+        st.title("シフッター")
         st.write("利用するモードを選んでください")
         st.button("従業員", on_click=lambda: ui_clear_and_go(ph, "employee"))
         st.button("管理者用", on_click=lambda: ui_clear_and_go(ph, "admin"))
     
 elif st.session_state.page == "employee":
     with (ph := st.empty()).container():
-        st.title('プロトタイプ')
-
-        st.header('従業員用ページ')
+        st.title('従業員用ページ')
 
         name = st.text_input("名前")
         text=input = st.text_input('希望日を入力してください')
@@ -105,9 +103,15 @@ elif st.session_state.page == "admin":
 
         #管理者用送信ボタンの処理
         if st.button('シフト一括作成'):
-            st.write('送信されました')
-            csv_text = gemini(main_prompt, sys_ins)
+            with st.spinner('送信されました!少々お待ちください...'):
+                csv_text = gemini(main_prompt, sys_ins)
             print(csv_text)
+            
+            csv_df = pd.read_csv(StringIO(csv_text))
+            csv_df = csv_df.replace(np.nan, "")
+            data = [csv_df.columns.tolist()] + csv_df.reset_index().values.tolist()
+            
+            sheet.update("D1", data)
 
             df = pd.read_csv(StringIO(csv_text))
             
@@ -126,10 +130,6 @@ elif st.session_state.page == "admin":
 
             st.subheader("シフト表")
             st.dataframe(pivot_df.fillna(""))
-            
-            data = [pivot_df.columns.tolist()] + pivot_df.reset_index().values.tolist()
-            
-            sheet.update("D1", data)
         
         st.button("ホームに戻る", on_click=lambda: ui_clear_and_go(ph, "home"))
 
